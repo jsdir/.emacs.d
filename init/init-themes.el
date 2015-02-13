cf8b0edade5bb97989050a2563aa1600b2b48ca1
Jason Sommer <jsdirv@gmail.com>
2015-02-13 02:54:08 -0600

    Switched to tomorrow theme

--- init/init-themes.el
+++ init/init-themes.el
@@ -1,5 +1,7 @@
-(require-package 'base16-theme)
+;;(require-package 'base16-theme)
+(require-package 'color-theme-sanityinc-tomorrow)
 
-(load-theme 'base16-chalk t)
+;; (load-theme 'base16-chalk t)
+(load-theme 'sanityinc-tomorrow-night t)
 
 (provide 'init-themes)
